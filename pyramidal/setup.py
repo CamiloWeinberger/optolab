9101d44f150a869a8d7b7343ec55c4e2007d2d65
CamiloWeinberger <camilo.weinberger.c@mail.pucv.cl>
2022-09-23 15:49:52 -0300

    Add setup

---
+++ pyramidal/setup.py
@@ -0,0 +1,12 @@
+# generate the setup.py file for the package called pyramidal with mit licence 
+ 
+from setuptools import setup
+ 
+setup(
+    name='pyramidal',
+    version='0.1',
+    description='A simple python package',
+    author='Camilo Weinberger',
+    packages=[''],
+)
+ 
